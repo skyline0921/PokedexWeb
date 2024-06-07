0d565438bc31d921a296090fea0ab11a4dbf7b22
Bryan <bryansrebello@gmail.com>
2024-06-07 17:25:34 -0300

    pokedex-web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import { POKEMONS } from './constants/pokemons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'pokedex-web';
+
+  pokemons = [
+    { 
+      id: 1,
+      name: "Bulbasaur",
+      number: "#001",
+      types: [
+        "grass",
+        "poison"
+      ]
+    },
+    { 
+      id: 2,
+      name: "Ivysaur",
+      number: "#002",
+      types: [
+        "grass",
+        "poison"
+      ]
+    },
+    { 
+      id: 3,
+      name: "Venusaur",
+      number: "#003",
+      types: [
+        "grass",
+        "poison"
+      ]
+    },
+    { 
+      id: 4,
+      name: "Charmander",
+      number: "#004",
+      types: [
+        "fire"
+      ]
+    },
+    { 
+      id: 5,
+      name: "Charmeleon",
+      number: "#005",
+      types: [
+        "fire"
+      ]
+    },
+    { 
+      id: 6,
+      name: "Charizard",
+      number: "#006",
+      types: [
+        "fire"
+      ]
+    },
+  ]
+
+  clickBulbasaur() {
+    return` 
+        <div class="descriptionPok"></div>`
+  }
 }
